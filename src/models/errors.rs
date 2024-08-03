06ef9fac2a9c202c9bd07a3c7a10f8b61950f040
nulluser0 <diuyueng@gmail.com>
2024-08-03 15:59:09 +1000

    fix response

--- src/models/errors.rs
+++ src/models/errors.rs
@@ -2,19 +2,15 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct HTTPValidationError {
-    detail: Option<ValidationError>,
+    detail: Option<Vec<ValidationError>>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ValidationError {
-    loc: ValidationErrorLocation, // Location
-    msg: String,                  // Message
-    r#type: String,               // Error Type
+    pub loc: Vec<ValidationErrorLocation>, // Location
+    pub msg: String,                       // Message
+    pub r#type: String,                    // Error Type
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-#[serde(untagged)]
-pub enum ValidationErrorLocation {
-    String(String),
-    Integer(u32),
-}
+pub struct ValidationErrorLocation {}
